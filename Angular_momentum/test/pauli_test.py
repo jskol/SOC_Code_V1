8697abb74d8375235e45144e3f1f5187aba0273d
jskol <skolimowski.jan@gmail.com>
2025-07-17 09:32:50 +0200

    folder_structure

---
+++ Angular_momentum/test/pauli_test.py
@@ -0,0 +1,9 @@
+from Angular_momentum.angular_momentum_basis import angular_momentum
+
+
+for key,value in angular_momentum['1/2'].items():
+	print(value)
+
+
+#test relacji komutacji w pętli
+#wrzucić na githuba
