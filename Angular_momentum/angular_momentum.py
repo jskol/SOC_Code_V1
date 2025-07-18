82d7e5a8f3740c0507a2def88839742404a52119
jskol <skolimowski.jan@gmail.com>
2025-07-18 11:30:57 +0200

    some small corrections and comments

--- Angular_momentum/angular_momentum.py
+++ Angular_momentum/angular_momentum.py
@@ -6,13 +6,29 @@ from generate_T_mat import generate_T_mat
 from ladder_operator import update_angular_momentum
 
 class AngularMomentum:
+    '''
+    AngularMomentum.basis object stores all L operators 
+    in a nested dictionary:
+    AngularMomentum.basis[l:float] -dictorary in a given l subspace
+    AngularMomentum.basis[l:float][x] - returns L_x matrix
+    AngularMomentum.basis[l:float][y] - returns L_y matrix
+    AngularMomentum.basis[l:float][z] - returns L_z matrix
+    '''
+    
     basis={}
     
     def __init__(self,l_values):
+        '''
+        Constructs set of L operators (L_x,L_y,L_z) for all
+        l-values given in l_values list 
+        '''
         for l in l_values:
             AngularMomentum.basis[l]=update_angular_momentum(l)
     
-    def print(self,l):
+    def print(self,l:float):
+        '''
+        Prints all L matrices for a given l-value
+        '''
         print("\nIn the L=%.1f subspace angular momentum operators are given by"%l)
         for name,mat in AngularMomentum.basis[l].items():
             print("L_%s:\n"%name, mat)
@@ -20,8 +36,8 @@ class AngularMomentum:
 
     def to_Cartesian(self):
         '''
-        Transforms L operator
-        from the spherical harmonics basis
+        Transforms all L operator
+        initially given in the spherical harmonics basis
         to the Cartesian space
         '''
 
@@ -37,3 +53,4 @@ if __name__=="__main__":
     l_vals=[0.5,1]
     AM=AngularMomentum(l_vals)
     AM.print(l_vals[1])
+    
