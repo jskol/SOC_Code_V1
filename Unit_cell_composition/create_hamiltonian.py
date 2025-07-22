065bb44c941b25f35daba6ce300553a65079d511
K4milJ <jamroszczyk.kamil@gmail.com>
2025-07-22 09:54:55 +0200

    Upgraded function calculating hamiltonian matrix.

--- Unit_cell_composition/create_hamiltonian.py
+++ Unit_cell_composition/create_hamiltonian.py
@@ -2,98 +2,70 @@ import numpy as np
 import re
 from itertools import islice
 
-def get_matrix_size(filename):
-	f=open(filename,'r')
-	element_index = 0
-	for line in f.readlines():
-		elements = re.sub(r'[\t ]+', ' ', line.strip()).split(' ')
-		if (len(elements) != 7):
-			continue
-		if (len(elements) == 7):
-			if (int(elements[3]) > int(element_index)): # i had to add int() to both, because it didn't work
-				element_index = elements[3]
-			else:
-				return element_index
-
-def save_to_file_with_zeros_end(elements, matrix_size, output_file):
-	# we change the values to 0.0
-	elements[5] = 0.0
-	elements[6] = 0.0
-	for i in range(1, matrix_size+1): # we iterate through remaining columns
-		elements[3] = matrix_size + i # increase the index value
-		#print("i, elem[3] = ", i, ", ", elements[3])
-		result_line = ' '.join(str(x) for x in elements)
-		output_file.write(result_line + '\n') #save line to file
-
-def save_to_file_with_zeros_beg(elements, matrix_size, output_file):
-	# we change the values to 0.0
-	elements[5] = 0.0
-	elements[6] = 0.0
-	elements[4] = int(elements[4]) + 1 # increase the index value
-	for i in range(1, matrix_size+1): # we iterate through first 'matrix_size' columns
-		elements[3] = i # increase the index value
-		result_line = ' '.join(str(x) for x in elements)
-		output_file.write(result_line + '\n') #save line to file
-
-def create_hamiltonian(spin_up_file):#, spin_down_file):
-	f=open(spin_up_file,'r')
-	#matrix_size = int(get_matrix_size(spin_up_file))
-	#print("matrix size = ", matrix_size)
-	#big_matrix = np.zeros((matrix_size**2, matrix_size**2), dtype=complex)
-	end_of_matrix_flag = False
-
-	with open(spin_up_file, 'r') as f:
-		lines = f.readlines()
-		num_wann = int(lines[1].strip())
-		nrpts = int(lines[2].strip())
-	matrix_size = 2*num_wann #one spin matrix
-	M = np.zeros((matrix_size, matrix_size), dtype=complex) #matrix for 'up' & 'down'
-	M_up = np.zeros((num_wann, num_wann), dtype=complex)
-	M_down = np.zeros((num_wann, num_wann), dtype=complex)
-
-	print("num_wann = ", num_wann)
-	print("nrpts = ", nrpts)
+def get_parameters(filename="wannier90_hr.dat"):
+	'''
+	Function returning num_wann and nrpts from the file.
+	'''
+	#Reads only 3 first lines of a file.
+	with open(filename) as f:
+		next(f)  # skip first line
+		num_wann = int(next(f).strip())
+		nrpts = int(next(f).strip())
+	return num_wann, nrpts
 
 
+def create_hamiltonian(*filenames):
+	'''
+	Function calculating hamiltonian for given files.
+	'''
+	#print("len(filenames) = ", len(filenames))
+	if (len(filenames) == 1):	#if we pas 1 file, both are the same
+		spin_up_file = filenames[0]
+		spin_down_file = filenames[0]
+	elif (len(filenames) == 2):
+		spin_up_file = filenames[0]
+		spin_down_file = filenames[1]
+	else:						#in other cases we have error
+		print("error")
+		exit(1)
+	print("s_up = ", spin_up_file)		###
+	print("s_down = ", spin_down_file)	###
 
-'''
-	for line in f.readlines():
-		elements = re.sub(r'[\t ]+', ' ', line.strip()).split(' ')
-		if (len(elements) != 7):
-			continue
-		if (len(elements) == 7):
-			with open('output_file.dat', 'a') as output_file:
-				output_file.write(re.sub(r'[\t ]+', ' ', line.strip()) + '\n') #save to file
-				if (int(elements[3]) == matrix_size):
-					save_to_file_with_zeros_end(elements, matrix_size, output_file)
-					if	(int(elements[3]) == 2*matrix_size and int(elements[4]) == matrix_size):
-						end_of_matrix_flag = True
-				if (int(elements[4]) == matrix_size and end_of_matrix_flag == True):
-					save_to_file_with_zeros_beg(elements, matrix_size, output_file)
-					output_file.write('hello \n') #save line to file
-					end_of_matrix_flag = False
-'''
+	spin_degeneracy = 2
 
-'''
-import numpy as np
+	num_wann, nrpts = get_parameters(spin_up_file)
+	print("num_wann, nrpts = ", num_wann, ", ", nrpts)	###
+	
+	skiplines = int(3 + np.ceil(nrpts/15.))
+	print("skiplines = ", skiplines)	###
 
-# Initialize 256x256 complex zero matrix
-N = 16
-big_matrix = np.zeros((N**2, N**2), dtype=complex)
+	M_up = np.loadtxt(spin_up_file, skiprows=skiplines)[:,3:]
+	M_down = np.loadtxt(spin_down_file, skiprows=skiplines)[:,3:]
+	print("M_up = \n", M_up) # it prints in scientific notation ###
 
-for i in range(N):
-    for j in range(N):
-        # Compute 16x16 block (replace this with your real computation)
-        block = np.zeros((N, N), dtype=complex)  # dummy block
+	M = np.zeros((spin_degeneracy * num_wann * nrpts, spin_degeneracy * num_wann * nrpts))#, dtype=complex)
+	print("M_shape = ", M.shape)	###
+	print("M = \n", M)	###
 
-        # Insert block into the big matrix
-        row_start = i * N
-        col_start = j * N
-        big_matrix[row_start:row_start+N, col_start:col_start+N] = block
+	for lines in M_up:
+		M[int(spin_degeneracy*lines[0]) - 2][int(spin_degeneracy*lines[1]) - 2] = lines[2] + 1j*lines[3]
+		print("[int(spin_degeneracy*lines[0]) - 1][int(spin_degeneracy*lines[1]) - 1]", int(spin_degeneracy*lines[0]), ", ", int(spin_degeneracy*lines[1]))
+		break
+	for lines in M_down:
+		M[int(spin_degeneracy*lines[0]) - 1][int(spin_degeneracy*lines[1]) - 1] = lines[2] + 1j*lines[3]
+		print("[int(spin_degeneracy*lines[0]) - 1][int(spin_degeneracy*lines[1]) - 1]", int(spin_degeneracy*lines[0]), ", ", int(spin_degeneracy*lines[1]))
+		break
+	# We have '-2' for spin up elements, and '-1' for spin down elements
+	# because ...
 
-'''
+	print("M = \n", M)	###	
+
+
+
+
+
 
 
 
 if (__name__=="__main__"):
-	create_hamiltonian("test/wannier90_down_hr.dat")
+	create_hamiltonian("test/wannier90_up_hr.dat", "test/wannier90_down_hr.dat")
