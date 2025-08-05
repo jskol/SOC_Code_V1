ae3f099abf61edf1a01918b17acdbc20113a114f
jskol <skolimowski.jan@gmail.com>
2025-08-05 11:34:08 +0200

    basis exchange ....

--- Basis_reordering/example.py
+++ Basis_reordering/example.py
@@ -10,17 +10,19 @@ for i in np.arange(Mat1.shape[0]):
     for j in np.arange(Mat1.shape[1]):
         Mat1[i][j]='<%s | H | %s >'%(str(i+1),str(j+1))
 
-print(Mat1)
-T_mat=np.eye(N,dtype=int)
+print("\nInitial ordering of elements\n",Mat1)
 
 #Exchange 1-3
 x,y=1,3
+
+T_mat=np.eye(N,dtype=int)
 T_mat[x-1][x-1]=0
 T_mat[y-1][y-1]=0
 T_mat[x-1][y-1]=1
 T_mat[y-1][x-1]=1
 
+Mat1_T=np.matrix(T_mat).T@Mat1@T_mat 
+print("\nAfter exchanging(%i with %i):\n"%(x,y),Mat1_T)
 
-print("\nAfter reordering:\n",np.matrix(T_mat).T@Mat1@T_mat)
 
 
