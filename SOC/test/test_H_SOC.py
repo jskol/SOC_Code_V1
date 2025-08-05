00137968ba8ea7800ca0ce00d120b72d296aa1c1
K4milJ <jamroszczyk.kamil@gmail.com>
2025-08-05 08:42:40 +0200

    fixed test_H_SOC.py

--- SOC/test/test_H_SOC.py
+++ SOC/test/test_H_SOC.py
@@ -11,9 +11,11 @@ from read_win import get_projections, get_composition, composition_wrapper
 from UnitCell import get_L_from_orbitals_set_name
 
 def check_difference(mat, mat2, size, ref):
-    mat[np.absolute(mat)<1e-6]=0
-    mat2[np.absolute(mat2)<1e-6]=0
-    if ~np.any(mat - mat2[ref:ref+size,ref:ref+size]):
+    mat[np.absolute(mat)<1e-6] = 0
+    mat2[np.absolute(mat2)<1e-6] = 0
+    diff_mat = mat - mat2[ref:ref+size,ref:ref+size]
+    diff_mat[np.absolute(diff_mat)<1e-6] = 0
+    if ~np.any(diff_mat):
         print("Test : ", colored("Passed", 'green'))
     else:
         exit("Test : Failed")
@@ -37,29 +39,18 @@ def calculate_H_SOC_ref(subspace, S_pauli):
 
 if __name__=="__main__":
     S_pauli=AngularMomentum(0.5)
-    S=AngularMomentum(0)
-    P=AngularMomentum(1)
-    D=AngularMomentum(2)
-    P.to_Cartesian(['px','py','pz'])
-    D.to_Cartesian(['dxy', 'dyz', 'dxz', 'dx2-y2', 'dz2'])
-
     spin_degeneracy = 2
 
-    filename = "../../Unit_cell_composition/test/wannier90_V3.win"
+    # filename = "../../Unit_cell_composition/test/wannier90_V2.win"
+    # filename = "../../Unit_cell_composition/test/wannier90_V3.win"
+    filename = "../../Unit_cell_composition/test/wannier90_V4.win"
     H_SOC = generate_H_SOC(filename)
+    H_SOC = np.array(H_SOC)
+
     ## TODO:
     # 1) Introduce compostion and atomatize testing
     # 2) each type of orbital printed in differentcolor
     # 3) test on two different *.win files
-
-    H_SOC_DS = np.kron(D.x(),S_pauli.x()) + np.kron(D.y(),S_pauli.y())+ np.kron(D.z(),S_pauli.z())
-    H_SOC_PS = np.kron(P.x(),S_pauli.x()) + np.kron(P.y(),S_pauli.y())+ np.kron(P.z(),S_pauli.z())
-    H_SOC_SS = np.kron(S.x(),S_pauli.x()) + np.kron(S.y(),S_pauli.y())+ np.kron(S.z(),S_pauli.z())
-
-    H_SOC_SS = np.array(H_SOC_SS)
-    H_SOC_PS = np.array(H_SOC_PS)
-    H_SOC_DS = np.array(H_SOC_DS)
-    H_SOC = np.array(H_SOC)
 
     comp = composition_wrapper(filename)
 
@@ -69,8 +60,6 @@ if __name__=="__main__":
         for subspace in split_orb:
             print_orbital(subspace)
             size = spin_degeneracy*len(subspace)
-
-            #for elem in len(subspace):
             H_SOC_ref = calculate_H_SOC_ref(subspace, S_pauli)
             check_difference(H_SOC_ref, H_SOC, size, ref_p)
-        ref_p += size
+            ref_p += size
