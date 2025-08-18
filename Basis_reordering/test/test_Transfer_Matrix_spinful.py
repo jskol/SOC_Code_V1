0013d6d9a0976c8b52427a2167c18c45e101fd31
jskol <skolimowski.jan@gmail.com>
2025-08-18 08:39:26 +0200

    Extended Transfer matrix function to deal with spinful basis  Trasfer_Matrix_spinful: Test -passed

--- Basis_reordering/test/test_Transfer_Matrix_spinful.py
+++ Basis_reordering/test/test_Transfer_Matrix_spinful.py
@@ -2,7 +2,7 @@ import sys,os
 import numpy as np
 
 sys.path.append('..')
-from Transfer_Matrix import Trasfer_Matrix
+from Transfer_Matrix import Trasfer_Matrix_spinful
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..','..', 'Unit_cell_composition'))
 from read_win import composition_wrapper
@@ -13,24 +13,30 @@ if __name__=="__main__":
     for win in win_names:
         
         print("\nReading from :", win)
-        l=Trasfer_Matrix(path_to_wins+win,print_details=False)
+        l=Trasfer_Matrix_spinful(path_to_wins+win,print_details=False)
         sq= l.T@l
         print("Square is giving unity: ", ~np.any(sq-np.eye(l.shape[0])))
         
         # Read the order from win
         comp = composition_wrapper(path_to_wins+win)
+        orb_list_print=[]
         orb_list=[]    
         for atom in comp:
+            for orb in atom.orbitals: #artificialy create spinful basis
+                orb_list_print.append(orb + "_up")
+                orb_list_print.append(orb + "_down")
             orb_list=orb_list+ atom.orbitals
+
         orb_list=np.array(orb_list)
-        print("from file", orb_list)
+        orb_list_print=np.array(orb_list_print)
+        print("from file", orb_list_print)
         
         #create a new reordered list
         new_orb_list=[]
-        reordered= l@np.arange(len(orb_list)) #vector after trasnformation
+        reordered= l@np.arange(len(orb_list_print)) #vector after trasnformation
         for new_pos in reordered:
-            new_orb_list.append(orb_list[int(new_pos)]) #change positions to str-values
-        print("after reordering" , np.array(new_orb_list))
+            new_orb_list.append(orb_list_print[int(new_pos)]) #change positions to str-values
+        print("after reordering" , np.array(new_orb_list))#
 
 
         
